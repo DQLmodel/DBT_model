81342ad31c74cf8d1e20dc2f0b09fe23fa6583bb
DQLmodel <QADBTTEST@outlook.com>
2025-12-17 18:16:39 +0530

    Adding

--- index.js
+++ index.js
@@ -946,7 +946,7 @@ const run = async () => {
           return;
         }
 
-        const metadataUrl = `http://44.233.244.28:8000/api/channel_action/github_action_metadata/`;
+        const metadataUrl = `http://44.233.244.28:8000/api/lineage/github_action_metadata/`;
         core.info(`[sendMetadataToDQLabs] Sending metadata to dqlabs: ${metadataUrl}`);
         
         const payload = {
