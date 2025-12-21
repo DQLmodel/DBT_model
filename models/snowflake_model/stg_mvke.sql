baf02cb1521671144e1e89645e314308e6ad0e60
DQLmodel <QADBTTEST@outlook.com>
2025-12-21 16:02:55 +0530

    Refactor SQL view by removing unused fields
    Removed casting of 'Mandt' to integer and trimming of 'Matnr'.

--- models/snowflake_model/stg_mvke.sql
+++ models/snowflake_model/stg_mvke.sql
@@ -2,8 +2,6 @@
 
 select
     -- Keys
-    cast("Mandt" as integer)                          as client,
-    trim("Matnr")                                     as material_id,
     trim("Vkorg")                                     as sales_org,
     lpad("Vtweg"::varchar, 2, '0')                     as distribution_channel,
 
