e8f59db1c573e599b84728586b3936419486b38c
DQLmodel <QADBTTEST@outlook.com>
2025-12-22 17:48:01 +0530

    Fix typo in column name from 'Mandt' to 'Mandte'

--- models/snowflake_model/stg_mvke.sql
+++ models/snowflake_model/stg_mvke.sql
@@ -2,7 +2,7 @@
 
 select
     -- Keys
-    cast("Mandt" as integer)                          as client,
+    cast("Mandte" as integer)                          as client,
     trim("Matnr")                                     as material_id,    
     trim("Vkorg")                                     as sales_org,
     lpad("Vtweg"::varchar, 2, '0')                     as distribution_channel,
