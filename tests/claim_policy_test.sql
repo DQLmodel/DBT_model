09af2e5f5c9019a051460e52f8afbd31ffb3251b
DQLmodel <QADBTTEST@outlook.com>
2026-03-19 09:37:59 +0530

    Update claim_policy_test.sql

--- tests/claim_policy_test.sql
+++ tests/claim_policy_test.sql
@@ -1,7 +1,8 @@
 {{ config(store_failures = true) }}
 select
     CUSTOMERID,
-    count(CLAIMAMOUNT) as total_amount
+    count(CLAIMAMOUNT) as total_amount,
+    UNIT_CHECK
 from {{ ref('join') }}
 group by 1
 having total_amount > 21
